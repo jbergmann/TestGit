06ab8a563101cec0be604a00d2f36e8b2e10e337
Jürgen Bergmann (Lenovo) <jbergmann@juergen-bergmann.at>
2020-03-01 09:29:32 +0100

    Test

--- TestGit/src/at/juergenbergmann/testgit/Start.java
+++ TestGit/src/at/juergenbergmann/testgit/Start.java
@@ -5,6 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is cool.");
+		System.out.println("And is fun.");
 	}
 
 }
