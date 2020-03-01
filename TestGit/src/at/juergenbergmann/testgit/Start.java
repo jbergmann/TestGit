abb1b9336e728832bf7b5795631b905fa64009fd
Jürgen Bergmann (Mannersdorf) <jbergmann@juergen-bergmann.at>
2020-03-01 08:57:54 +0100

    First Commit

---
+++ TestGit/src/at/juergenbergmann/testgit/Start.java
@@ -0,0 +1,10 @@
+package at.juergenbergmann.testgit;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun.");
+	}
+
+}
